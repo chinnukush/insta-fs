2dfa7cfd9bac67a33ca48d2f0a89e351ec0c49fb
chinnukush <chinnukushal123@gmail.com>
2025-10-02 14:41:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "6566268406")) # Owner id
 #--------------------------------------------
 PORT = os.environ.get("PORT", "8001")
 #--------------------------------------------
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://anikush8310:4wTqyEhgZh96x0GV@cluster0.n1clhjc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://anikush8310_db_user:A5qknHVIUlBegUqU@cluster0.9fb2nmj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluooo")
 #--------------------------------------------
 FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "0"))  # 0 means no expiry
